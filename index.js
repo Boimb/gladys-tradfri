d75b9bd21ce76199b9019f4500432c38754cc9cd
Boimb <atboimb@gmail.com>
2018-08-21 02:13:05 +0200

    Add init at startup

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 const init = require('./lib/init')
 const install = require('./lib/install')
 const setup = require('./lib/setup')
+const connect = require('./lib/connect')
 
 module.exports = function(sails) {
-
+  gladys.on('ready', function(){
+    init();
+  });
   return {
     install,
     setup,
-    init
+    init,
+    connect
   }
 };
