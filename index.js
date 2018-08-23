a55c9f0f301e78fa1af2aa2951233fb0ea7e9d34
Boimb <atboimb@gmail.com>
2018-08-24 00:52:39 +0200

    Del useless install method. Added Commas...

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-
-const init = require('./lib/init')
-const install = require('./lib/install')
-const setup = require('./lib/setup')
+const init = require('./lib/init');
+const setup = require('./lib/setup');
 
 module.exports = function(sails) {
   gladys.on('ready', function(){
     init();
   });
+
   return {
-    install,
     setup,
     init
   }
