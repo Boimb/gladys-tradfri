0aed0473c16f8b7646afcd75fd1cf00c3965f098
Boimb <atboimb@gmail.com>
2018-09-03 01:39:17 +0200

    Wip Travis integration

--- test/connectTest.js
+++ test/connectTest.js
@@ -5,7 +5,7 @@ const gladysMock = require('gladys-node-api-mock')
 const sailsGlobal = require('./mocks/sails')
 const {discoveredGatewayMock, TradfriClientMock} = require('./mocks/tradfriClient')
 const connect = require('../lib/connect')
-const {TRADFRI_SECRET} = require ('../lib/const')
+const {TRADFRI_SECRET, IDENTITY, PSK} = require ('../lib/const')
 
 beforeEach(function() {
   sails = sailsGlobal
@@ -23,7 +23,6 @@ describe('CONNECT TESTS', () => {
           assert(err.message === 'Could not find any TRADFRI gateway', 'Attended rejection')
         })
     })
-
   })
 
   describe('Using validSecret', () => {
@@ -43,6 +42,19 @@ describe('CONNECT TESTS', () => {
         .catch(err => {
           assert(true === false, 'Should not have been rejected')
         })
+    })
+
+    it('Should connect if valid credentials', () => {
+      // Set a valid secret
+      gladys.param.setValue({name: IDENTITY, value: TradfriClientMock.VALID_IDENTITY})
+      gladys.param.setValue({name: PSK, value: TradfriClientMock.VALID_PSK})
+      return connect(discoveredGatewayMock)
+      .then(client => {
+        assert(client instanceof TradfriClient, 'Got client')
+      })
+      .catch(err => {
+        assert(true === false, 'Should not have been rejected')
+      })
     })
   })
 })
